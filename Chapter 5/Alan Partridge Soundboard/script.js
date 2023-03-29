a0d1a1e3ac4e26a2624f9b7650d6a3ba5751d4fb
Kristian <kristkross30@gmail.com>
2023-03-30 00:27:46 +0400

    commit

---
+++ Chapter 5/Alan Partridge Soundboard/script.js
@@ -0,0 +1,19 @@
+function sound1(){
+    var snd = new Audio('BTFIL.mp3')
+    snd.play()
+}
+
+function sound2(){
+    var snd = new Audio('Dan.mp3')
+    snd.play()
+}
+
+function sound5(){
+    var snd = new Audio('Jurrasic.mp3')
+    snd.play()
+}
+
+function sound6(){
+    var snd = new Audio('Cheese.mp3')
+    snd.play()
+}
