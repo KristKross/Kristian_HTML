c47d8dc3d72f58d39dfde481a16371a8d50d6b28
Kristian <kristkross30@gmail.com>
2023-04-27 15:46:27 +0400

    commit

--- Chapter 6/Temperature Calculator/script.js
+++ Chapter 6/Temperature Calculator/script.js
@@ -14,71 +14,101 @@ function convert() {
         var answer = numValue;
         var formatNum = Math.floor(answer * 100)/100;
         document.getElementById("output").innerHTML = formatNum + "°C"
-    } else if(value === "1" && convertedValue === "2") {
+    } 
+    
+    else if(value === "1" && convertedValue === "2") {
         var answer = (numValue * 1.8) + 32;
         var formatNum = Math.floor(answer * 100)/100;
         document.getElementById("output").innerHTML = formatNum + "°F"
-    } else if(value === "1" && convertedValue === "3") {
+    } 
+    
+    else if(value === "1" && convertedValue === "3") {
         var answer = numValue + 273.15;
         var formatNum = Math.floor(answer * 100)/100;
         document.getElementById("output").innerHTML = formatNum + "K"
-    } else if(value === "1" && convertedValue === "4") {
+    } 
+    
+    else if(value === "1" && convertedValue === "4") {
         var answer = (numValue * 1.8) + 491.67;
         var formatNum = Math.floor(answer * 100)/100;
         document.getElementById("output").innerHTML = formatNum + "°R"
 
 
 
-    } else if(value === "2" && convertedValue === "1") {
+    } 
+    
+    else if(value === "2" && convertedValue === "1") {
         var answer = (numValue - 32) * (5/9);
         var formatNum = Math.floor(answer * 100)/100;
         document.getElementById("output").innerHTML = formatNum + "°C"
-    } else if(value === "2" && convertedValue === "2") {
+    } 
+    
+    else if(value === "2" && convertedValue === "2") {
         var answer = numValue;
         var formatNum = Math.floor(answer * 100)/100;
         document.getElementById("output").innerHTML = formatNum + "°F"
-    } else if(value === "2" && convertedValue === "3") {
+    } 
+    
+    else if(value === "2" && convertedValue === "3") {
         var answer = (numValue - 32) * (5/9) + 273.15;
         var formatNum = Math.floor(answer * 100)/100;
         document.getElementById("output").innerHTML = formatNum + "K"
-    } else if(value === "2" && convertedValue === "4") {
+    } 
+    
+    else if(value === "2" && convertedValue === "4") {
         var answer = numValue + 459.67;
         var formatNum = Math.floor(answer * 100)/100;
         document.getElementById("output").innerHTML = formatNum + "°R"
 
 
 
-    } else if(value === "3" && convertedValue === "1") {
+    } 
+    
+    else if(value === "3" && convertedValue === "1") {
         var answer = numValue - 273.15;
         var formatNum = Math.floor(answer * 100)/100;
         document.getElementById("output").innerHTML = formatNum + "°C"
-    } else if(value === "3" && convertedValue === "2") {
+    } 
+    
+    else if(value === "3" && convertedValue === "2") {
         var answer = (numValue - 273.15) * 1.8 + 32;
         var formatNum = Math.floor(answer * 100)/100;
         document.getElementById("output").innerHTML = formatNum + "°F"
-    } else if(value === "3" && convertedValue === "3") {
+    } 
+    
+    else if(value === "3" && convertedValue === "3") {
         var answer = numValue;
         var formatNum = Math.floor(answer * 100)/100;
         document.getElementById("output").innerHTML = formatNum + "K"
-    } else if(value === "3" && convertedValue === "4") {
+    } 
+    
+    else if(value === "3" && convertedValue === "4") {
         var answer = (numValue * 9) / 5
         var formatNum = Math.floor(answer * 100)/100;
         document.getElementById("output").innerHTML = formatNum + "°R"
+    } 
+    
 
-
-    } else if(value === "4" && convertedValue === "1") {
+    
+    else if(value === "4" && convertedValue === "1") {
         var answer = (numValue * (5/9) - 273.15);
         var formatNum = Math.floor(answer * 100)/100;
         document.getElementById("output").innerHTML = formatNum + "°C"
-    } else if(value === "4" && convertedValue === "2") {
+    } 
+    
+    else if(value === "4" && convertedValue === "2") {
         var answer = numValue - 459.67;
         var formatNum = Math.floor(answer * 100)/100;
         document.getElementById("output").innerHTML = formatNum + "°F"
-    } else if(value === "4" && convertedValue === "3") {
+    } 
+    
+    else if(value === "4" && convertedValue === "3") {
         var answer = numValue * (5/9);
         var formatNum = Math.floor(answer * 100)/100;
         document.getElementById("output").innerHTML = formatNum + "K"
-    } else if(value === "4" && convertedValue === "4") {
+    } 
+    
+    else if(value === "4" && convertedValue === "4") {
         var answer = numValue
         var formatNum = Math.floor(answer * 100)/100;
         document.getElementById("output").innerHTML = formatNum + "°R"
